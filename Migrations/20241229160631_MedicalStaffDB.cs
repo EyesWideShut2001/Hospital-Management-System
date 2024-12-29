82272326f6d7228f30cc00799f688d7cf6544f60
ana <anaionita.fly@gmail.com>
2024-12-29 18:09:56 +0200

    add full list of medical Staff to db

---
+++ Migrations/20241229160631_MedicalStaffDB.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Hospital_Managment.Migrations
+{
+    /// <inheritdoc />
+    public partial class MedicalStaffDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "MedicalStaffs",
+                columns: new[] { "Id", "AdminId", "DepartmentId", "PasswordHash", "Username" },
+                values: new object[,]
+                {
+                    { 2, 1, 1, "$2a$11$9Vhk6FakLIPw2oqITiyWd.5yT1RKRbg7FwZNBurvD9nLciLfmjoWm", "Carina" },
+                    { 3, 1, 1, "$2a$11$xJEYrgnETzvowSCYAtPdlu13jhRZ6P02EZDF9d/XAOpW2Tq1nuSSi", "Flavius" },
+                    { 4, 1, 1, "$2a$11$u5CL6ypkZPwLpRoMoN2GoO4pQ2N8HzDdvW1QD7zuIDDw9/lRnGlYy", "George" },
+                    { 5, 1, 2, "$2a$11$2JQHdcU.6ZfS.WX5pLVPluy7uJq4kB/Cn0OqpwXpzKjcb8mL1Mu/S", "Maria" },
+                    { 6, 1, 2, "$2a$11$n2lPnRp6jVuKY7219WD5seyMEQzq45jxc4AZzLPSyy5opKe2ypq7O", "Ilie" },
+                    { 7, 1, 2, "$2a$11$FgkiwL3dvqrvu7O9mKuGaue8MucA5ImP283mKDCdo0kbY3fh7Jsum", "Vlad" },
+                    { 8, 2, 1, "$2a$11$G6tVf7hR8ni7trHf0WZoXOUH7UbjDomgwos3Hrp1jMI7asE0vwM6a", "Cristi" },
+                    { 9, 2, 1, "$2a$11$eU0vuoBKQt5kOMVx3SPO8upqtosPy/tFKrFvMgUbAfvRem/FB5dcq", "Serban" },
+                    { 10, 2, 1, "$2a$11$o0UR5DQiiQq9zFm/QG28I.QCgxqZtgKhtPuklssaOvI7SiqHn3Zje", "Tudor" },
+                    { 11, 2, 1, "$2a$11$g4zc2Jfj3I3RTUvft9Xj8elsh/Wz9Sdi8UW22cApp.tFf6RdPinCy", "Alexandra" },
+                    { 12, 3, 1, "$2a$11$qrl93QvU3uPGlQIvIkfTQO3BrvYRGUC6Y/u0h7IKp4m9kPRNR/HG6", "Marc" },
+                    { 13, 3, 1, "$2a$11$sTmnkC5GP3Dh49YA8Hix0edhTrpKNhuWAm7MN0uqle6c5CVTOaOYe", "Diana" },
+                    { 14, 3, 1, "$2a$11$pau9CtSFoAs.mBn83PNW1OkonJWEheD4UKGCRHuOIEyLcmUDsHfsK", "Ana" },
+                    { 15, 3, 1, "$2a$11$cp74W4PcfUh6FiAvAgDon.ihBt2cbBDk5hB1Vtb5oY2tnxtmntvVe", "Dan" },
+                    { 16, 3, 1, "$2a$11$vreICMp6.M3Z9MwUEY/TOupC05lYNJASV5S63x9LbYERBFPGtCI8G", "Filip" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "MedicalStaffs",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "MedicalStaffs",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "MedicalStaffs",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "MedicalStaffs",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "MedicalStaffs",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "MedicalStaffs",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "MedicalStaffs",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "MedicalStaffs",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "MedicalStaffs",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "MedicalStaffs",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "MedicalStaffs",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "MedicalStaffs",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "MedicalStaffs",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "MedicalStaffs",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "MedicalStaffs",
+                keyColumn: "Id",
+                keyValue: 16);
+        }
+    }
+}
--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -64,7 +64,98 @@ namespace HospitalManagment.Models
                    Id = 1, AdminId = 1, Username = "Sara",
                    PasswordHash = "$2a$12$RvBnjdK5vod8qb02w0ZxzuafaeDZ5hUyjM.x2E1tEsardDL5tiA0y",
                    DepartmentId=1
-               }
+               },
+                new MedicalStaff
+               {
+                   Id = 2, AdminId = 1, Username = "Carina",
+                   PasswordHash = "$2a$11$9Vhk6FakLIPw2oqITiyWd.5yT1RKRbg7FwZNBurvD9nLciLfmjoWm",
+                   DepartmentId=1
+               },
+               new MedicalStaff
+               {
+                   Id = 3, AdminId = 1, Username = "Flavius",
+                   PasswordHash = "$2a$11$xJEYrgnETzvowSCYAtPdlu13jhRZ6P02EZDF9d/XAOpW2Tq1nuSSi",
+                   DepartmentId=1
+               },
+               new MedicalStaff
+               {
+                   Id = 4, AdminId = 1, Username = "George",
+                   PasswordHash = "$2a$11$u5CL6ypkZPwLpRoMoN2GoO4pQ2N8HzDdvW1QD7zuIDDw9/lRnGlYy",
+                   DepartmentId=1
+               },
+               new MedicalStaff
+               {
+                   Id = 5, AdminId = 1, Username = "Maria",
+                   PasswordHash = "$2a$11$2JQHdcU.6ZfS.WX5pLVPluy7uJq4kB/Cn0OqpwXpzKjcb8mL1Mu/S",
+                   DepartmentId=2
+               },
+           new MedicalStaff
+               {
+                   Id = 6, AdminId = 1, Username = "Ilie",
+                   PasswordHash = "$2a$11$n2lPnRp6jVuKY7219WD5seyMEQzq45jxc4AZzLPSyy5opKe2ypq7O",
+                   DepartmentId=2
+               },
+           new MedicalStaff
+           {
+               Id = 7, AdminId = 1, Username = "Vlad",
+               PasswordHash = "$2a$11$FgkiwL3dvqrvu7O9mKuGaue8MucA5ImP283mKDCdo0kbY3fh7Jsum",
+               DepartmentId=2
+           },
+           new MedicalStaff
+           {
+               Id = 8, AdminId = 2, Username = "Cristi",
+               PasswordHash = "$2a$11$G6tVf7hR8ni7trHf0WZoXOUH7UbjDomgwos3Hrp1jMI7asE0vwM6a",
+               DepartmentId=1
+           },
+           new MedicalStaff
+           {
+               Id = 9, AdminId = 2, Username = "Serban",
+               PasswordHash = "$2a$11$eU0vuoBKQt5kOMVx3SPO8upqtosPy/tFKrFvMgUbAfvRem/FB5dcq",
+               DepartmentId=1
+           },
+           new MedicalStaff
+           {
+               Id = 10, AdminId = 2, Username = "Tudor",
+               PasswordHash = "$2a$11$o0UR5DQiiQq9zFm/QG28I.QCgxqZtgKhtPuklssaOvI7SiqHn3Zje",
+               DepartmentId=1
+           }, 
+               new MedicalStaff
+                   {
+                       Id = 11, AdminId = 2, Username = "Alexandra",
+                       PasswordHash = "$2a$11$g4zc2Jfj3I3RTUvft9Xj8elsh/Wz9Sdi8UW22cApp.tFf6RdPinCy",
+                       DepartmentId=1
+                   },
+                new MedicalStaff
+           {
+               Id = 12, AdminId = 3, Username = "Marc",
+               PasswordHash = "$2a$11$qrl93QvU3uPGlQIvIkfTQO3BrvYRGUC6Y/u0h7IKp4m9kPRNR/HG6",
+               DepartmentId=1
+           },
+                new MedicalStaff
+                {
+                    Id = 13, AdminId = 3, Username = "Diana",
+                    PasswordHash = "$2a$11$sTmnkC5GP3Dh49YA8Hix0edhTrpKNhuWAm7MN0uqle6c5CVTOaOYe",
+                    DepartmentId=1
+                },
+                new MedicalStaff
+                {
+                    Id = 14, AdminId = 3, Username = "Ana",
+                    PasswordHash = "$2a$11$pau9CtSFoAs.mBn83PNW1OkonJWEheD4UKGCRHuOIEyLcmUDsHfsK",
+                    DepartmentId=1
+                },
+                new MedicalStaff
+                {
+                    Id = 15, AdminId = 3, Username = "Dan",
+                    PasswordHash = "$2a$11$cp74W4PcfUh6FiAvAgDon.ihBt2cbBDk5hB1Vtb5oY2tnxtmntvVe",
+                    DepartmentId=1
+                },
+                new MedicalStaff
+                {
+                    Id = 16, AdminId = 3, Username = "Filip",
+                    PasswordHash = "$2a$11$vreICMp6.M3Z9MwUEY/TOupC05lYNJASV5S63x9LbYERBFPGtCI8G",
+                    DepartmentId=1
+                }
+                
            );
 
 
